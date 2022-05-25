3889d6aa7917096c63541db8d940455fcc699b3b
Jonathan McGrath <octoberizm@gmail.com>
2022-05-24 18:52:00 -0800

    Adding dependencies

---
+++ frontend/server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+
+var app = express();
+
+app.get('/', function (req, res){
+    res.send('Hello');
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example App, at http://%s:%s", host, port)
+})

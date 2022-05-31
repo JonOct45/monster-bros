1d650e60578a6ee7b2be4a8e3e3d7e52d1674721
Jonathan McGrath <octoberizm@gmail.com>
2022-05-31 15:22:15 -0800

    Adding HTML file, and added the History/Navigation/Location and Character JS - CSS files.

---
+++ frontend/src/history.js
@@ -0,0 +1,14 @@
+import "./css/history.css"
+
+export function History() {
+
+
+
+    return(
+        <div className="history">
+            <section className="history-section">
+            <h1>History</h1>
+            </section>
+            </div>
+    )
+}
---
+++ frontend/src/navigation.js
@@ -0,0 +1,12 @@
+import "./css/navigation.css"
+
+export function Navigation() {
+
+    return(
+        <div className="navigation">
+            <section className="navigation-section">
+            <h1>Navigation</h1>
+            </section>
+            </div>
+    )
+}
